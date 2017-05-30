571bc0e62a67744cbb50ad6662c8a498aa9754fa
David Johnston <davidajohnston2000@gmail.com>
2017-05-30 23:02:19 +0100

    Add facebook authentication

--- src/main/java/uk/co/dajohnston/leaguetracker/Application.java
+++ src/main/java/uk/co/dajohnston/leaguetracker/Application.java
@@ -2,12 +2,25 @@ package uk.co.dajohnston.leaguetracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 @SuppressWarnings("PMD.UseUtilityClass")
-public class Application {
+public class Application extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**").authorizeRequests().antMatchers("/", "/login**")
+                .permitAll().anyRequest().authenticated().and().logout().logoutSuccessUrl("/")
+                .permitAll().and().csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
     }
 }
