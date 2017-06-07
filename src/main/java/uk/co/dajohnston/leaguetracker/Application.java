12458b53d624e7d35352191c3b9e8417f6630d7d
David Johnston <davidajohnston2000@gmail.com>
2017-06-07 22:22:18 +0100

    Use separate auth server

--- src/main/java/uk/co/dajohnston/leaguetracker/Application.java
+++ src/main/java/uk/co/dajohnston/leaguetracker/Application.java
@@ -6,6 +6,7 @@ import javax.servlet.Filter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -24,94 +25,11 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.filter.CompositeFilter;
 
 @SpringBootApplication
-@EnableOAuth2Client
+@EnableOAuth2Sso
 @SuppressWarnings("PMD.UseUtilityClass")
-public class Application extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    OAuth2ClientContext oauth2ClientContext;
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.antMatcher("/**").addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class).authorizeRequests()
-                .antMatchers("/", "/login**").permitAll().anyRequest().authenticated().and().logout().logoutSuccessUrl("/").permitAll()
-                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and();
-    }
-
-    private Filter ssoFilter() {
-        OAuth2ClientAuthenticationProcessingFilter facebookFilter = createOAuth2Filter("/login/facebook", facebook(),
-                facebookResource().getUserInfoUri());
-        OAuth2ClientAuthenticationProcessingFilter githubFilter = createOAuth2Filter("/login/github", github(),
-                githubResource().getUserInfoUri());
-        OAuth2ClientAuthenticationProcessingFilter googleFilter = createOAuth2Filter("/login/google", google(),
-                googleResource().getUserInfoUri());
-
-        List<Filter> filters = new ArrayList<>();
-        filters.add(facebookFilter);
-        filters.add(githubFilter);
-        filters.add(googleFilter);
-        CompositeFilter filter = new CompositeFilter();
-        filter.setFilters(filters);
-        return filter;
-
-    }
-
-    private OAuth2ClientAuthenticationProcessingFilter createOAuth2Filter(String url, AuthorizationCodeResourceDetails resourceDetails,
-            String userInfoUri) {
-        UserInfoTokenServices tokenServices = new UserInfoTokenServices(userInfoUri, resourceDetails.getClientId());
-        OAuth2RestTemplate template = new OAuth2RestTemplate(resourceDetails, oauth2ClientContext);
-        tokenServices.setRestTemplate(template);
-        OAuth2ClientAuthenticationProcessingFilter filter = new OAuth2ClientAuthenticationProcessingFilter(url);
-        filter.setRestTemplate(template);
-        filter.setTokenServices(tokenServices);
-        return filter;
-    }
-
-    @Bean
-    @ConfigurationProperties("facebook.client")
-    public AuthorizationCodeResourceDetails facebook() {
-        return new AuthorizationCodeResourceDetails();
-    }
-
-    @Bean
-    @ConfigurationProperties("facebook.resource")
-    public ResourceServerProperties facebookResource() {
-        return new ResourceServerProperties();
-    }
-
-    @Bean
-    @ConfigurationProperties("github.client")
-    public AuthorizationCodeResourceDetails github() {
-        return new AuthorizationCodeResourceDetails();
-    }
-
-    @Bean
-    @ConfigurationProperties("github.resource")
-    public ResourceServerProperties githubResource() {
-        return new ResourceServerProperties();
-    }
-
-    @Bean
-    @ConfigurationProperties("google.client")
-    public AuthorizationCodeResourceDetails google() {
-        return new AuthorizationCodeResourceDetails();
-    }
-
-    @Bean
-    @ConfigurationProperties("google.resource")
-    public ResourceServerProperties googleResource() {
-        return new ResourceServerProperties();
-    }
-
-    @Bean
-    public FilterRegistrationBean oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
-        FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setFilter(filter);
-        registration.setOrder(-100);
-        return registration;
     }
 }
